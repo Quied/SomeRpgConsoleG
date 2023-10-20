734527773621d75eb988cbd2f3a4cc2f3522d5c3
quied <dexflame3@gmail.com>
2023-10-20 09:39:17 -0400

    lab3

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace AppConsole {
+
+    enum TimeFrame {
+        Year, 
+        TwoYears,
+        Long,
+    };
+
+    class Person {}
+
+    class Paper {
+        
+        Paper(){
+            this.titleName = "Some title";
+            this.author = null;
+            this.publicationDate = new DateTime(2015,2,2);
+        }
+
+        Paper(string name, Person person, DateTime date) { 
+            this.titleName = name;
+            this.author = person;
+            this.publicationDate = date;
+        }
+
+        public string titleName;
+        public Person author;
+        public DateTime publicationDate;
+    }
+    class ResearchTeam {
+        private string researchTopic;
+        private string Organization;
+        private int number;
+        private TimeFrame reseatchTime;
+    
+    }
+
+
+    class Entry {
+
+        static void Main(){
+            Entry entry = new Entry();
+        }
+
+    }
+
+}
