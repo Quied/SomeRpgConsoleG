a219df4dd0c2d2084de5567204f85a4beca4d859
Quied <82113495+Quied@users.noreply.github.com>
2024-03-22 09:04:07 +0200

    Add files via upload

--- lab2.php
+++ lab2.php
@@ -1,11 +1,30 @@
 <?php
-$a = -9.86;
-$x = 0.56;
+function countDigits($n) {
+    $count = 0;
+    $sum = 0;
+    $hasDigit3 = false;
 
-$numerator = pow($x, 3) - $x * cos($x);
-$denominator = pow($a + $x, 2);
+    $digits = str_split((string)$n);
 
-$result = $numerator / $denominator;
+    foreach ($digits as $digit) {
+        $count++;
 
-echo "Result: " . $result;
+        $sum += $digit;
+
+
+        if ($digit == 3) {
+            $hasDigit3 = true;
+        }
+    }
+
+    return [$count, $sum, $hasDigit3];
+}
+
+$n = 12345;
+
+$result = countDigits($n);
+
+echo "Кількість цифр у числі $n: " . $result[0] . "\n";
+echo "Сума цифр числа $n: " . $result[1] . "\n";
+echo "Число $n " . ($result[2] ? 'включає' : 'не включає') . " цифру '3'.\n";
 ?>
